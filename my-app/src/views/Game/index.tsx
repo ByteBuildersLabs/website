0106d363375306493f7c7be4a66db08ae44b501d
RolandoDrRobot <rolando.arguello.email@gmail.com>
2024-07-28 23:27:53 -0600

    Ok let's try the game now

--- my-app/src/views/Game/index.tsx
+++ my-app/src/views/Game/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Play from '../../components/Play';
+import RoadmapMonth from '../../components/RoadmapMonth';
 import './main.css';
 
 function Game() {
@@ -9,8 +11,16 @@ function Game() {
           <span>Byte Builders Labs</span>
           <h2>Check our progress<span className="d-block">Play Byte Beasts</span></h2>
         </div>
-
-        
+        <Play />
+        <RoadmapMonth
+          completed={false}
+          title="Upcoming work - August 2024"
+          title2="Create First NFTs Byte Beasts Collection - In Progress"
+          subtitle1="NFT Integration: Develop and test the NFT creation and trading system on Starknet"
+          subtitle2="Byte Beasts Design: Create the initial set of Byte Beasts, focusing on the five elemental types"
+          subtitle3="Art and Graphics: Begin the development of game assets and visual design"
+          subtitle4="Team Assembly Mechanism: Implement the feature for players to assemble teams of Byte Beasts"
+        />
       </div>
     </>
   )
